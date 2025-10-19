981af4241f893cbe2fbedbaf87d7cb01baca963a
desuyume <77desss@gmail.com>
2025-10-19 12:16:38 +0200

    chore: add react-compiler

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		reactCompiler: true
+	},
 	transpilePackages: ['@workify/ui'],
 	env: {
 		API_URL: process.env.API_URL,
